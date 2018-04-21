ffb6160891b8e097f7f50522faa54d974e84ff36
David R <mineawesomeman@gmail.com>
2018-04-21 17:34:12 -0400

    New Stuff

---
+++ Dayvid's Project/Assets/moveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveScript : MonoBehaviour {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RectTransform trans = GetComponent<RectTransform>();
+        Vector3 loc = trans.localPosition;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<RectTransform>().localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<RectTransform>().localPosition = new Vector3(loc.x + speed, loc.y, loc.z);
+
+        }
+
+       
+    }
+}
